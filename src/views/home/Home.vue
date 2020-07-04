3849adff9570caaab777ae5e93f322d2a6aac004
周子兴 <125139887@qq.com>
2020-07-04 19:59:52 +0800

    代码提交

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -12,6 +12,8 @@
     <button @click="connect">连接</button>
     <br />
     <button @click="send">send</button>
+    <br />
+    <button @click="play">play</button>
   </div>
 </template>
 
@@ -20,6 +22,8 @@
 //例如：import 《组件名称》 from '《组件路径》';
 import SockJS from 'sockjs-client';
 import Stomp from 'stompjs';
+
+import { voicePrompt } from '@/common/voicePrompt'
 
 export default {
   //import引入的组件需要注入到对象中才能使用
@@ -30,15 +34,38 @@ export default {
       msg: '哈哈哈哈',
       myStompClient: null,
       uid: null,
-      toUid: null
+      toUid: null,
     }
   },
   //监听属性 类似于data概念
-  computed: {},
+  computed: {
+    socketUrl() {
+      // return 'http://172.16.108.202:5622/im/conn?uid=' + this.uid;
+      return 'http://192.168.31.28:8080/im/conn?uid=' + this.uid;
+    }
+  },
   //监控data中的数据变化
   watch: {},
   //方法集合
   methods: {
+    play() {
+      const synth = window.speechSynthesis
+      console.log(synth.getVoices())
+      const msg = new SpeechSynthesisUtterance()
+      msg.voice = synth.getVoices()[22] //设置人声音
+      // msg.text = '所以暂时将你眼睛闭了起来,黑暗之中漂浮我的期待'
+      msg.text = '故事的小黄花,从出生那年就飘着,童年的荡秋千,随记忆一直晃到现在'
+
+      msg.lang = 'zh-CN'
+      msg.volume = 1//0-1 音量大小
+      msg.rate = 0.75 //0.1-10 播放语速
+      msg.pitch = 1//0-2 音调高低
+
+      synth.speak(msg)
+
+
+      // voicePrompt('哇,好漂亮的A呀,你是C吗')
+    },
     send() {
       var params = {
         toUid: this.toUid,
@@ -49,38 +76,53 @@ export default {
       this.myStompClient.send('/im/send2user', {}, JSON.stringify(params))
     },
     connect() {
-      let socket = new SockJS('http://localhost:8080/im/conn?uid=' + this.uid);
+      let socket = new SockJS(this.socketUrl);
       let stompClient = Stomp.over(socket);
-      this.myStompClient = stompClient
-      stompClient.connect({}, function (frame) {
-        stompClient.subscribe('/user/msg', function (message) {
-          console.log("订阅个人消息");
-          console.warn(message.body);
-          console.warn(JSON.parse(message.body));
-        });
-      });
+      stompClient.connect({}, frame => {
+        this.myStompClient = stompClient
+        //订阅个人消息
+        this.subscribeUser()
+      }, () => {
+        console.log("断开了")
+        this.reconnect(this.socketUrl, this.subscribeUser)
+      })
+    },
+    //订阅个人消息
+    subscribeUser() {
+      this.myStompClient && this.myStompClient.subscribe('/user/' + this.uid + '/msg', function (message) {
+        console.log("订阅个人消息");
+        console.warn(message.body);
+        console.warn(JSON.parse(message.body));
+      })
+    },
+    // 断开重连使用定时器定时连接服务器
+    reconnect(socketUrl, successCallback) {
+      console.info('开始重连')
+      let connected = false
+      const reconInv = setInterval(() => {
+        console.info('in interval' + Math.random())
+        let socket = new SockJS(socketUrl)
+        let stompClient = Stomp.over(socket)
+        stompClient.connect({}, (frame) => {
+          this.myStompClient = stompClient
+          console.info('reconnected success')
+          // 连接成功，清除定时器
+          clearInterval(reconInv)
+          connected = true
+          successCallback()
+        }, () => {
+          console.info('reconnect failed')
+          console.info('connected:' + connected)
+          if (connected) {
+            this.reconnect(this.socketUrl, this.subscribeUser)
+          }
+        })
+      }, 5000)
     }
   },
   //生命周期 - 创建完成（可以访问当前this实例）
   created() {
-    // let socket = new SockJS('http://localhost:8080/im/conn?uid=' + '0772');
-    // let stompClient = Stomp.over(socket);
-    // this.myStompClient = stompClient
-    // stompClient.connect({}, function (frame) {
-    //   stompClient.subscribe('/user/msg', function (message) {
-    //     console.warn(message.body);
-    //     console.warn(JSON.parse(message.body));
-    //   });
 
-    //   // var params = {
-    //   //   uid: "0772",
-    //   //   toUid: "0772",
-    //   //   content: "456465"
-    //   // }
-    //   // //'/im'对应setApplicationDestinationPrefixes
-    //   // //'send2user' 对应控制器里面MessageMapping
-    //   // stompClient.send('/im/send2user', {}, JSON.stringify(params))
-    // });
   },
   //生命周期 - 挂载完成（可以访问DOM元素）
   mounted() {
